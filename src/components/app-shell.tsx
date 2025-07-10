fb6ecf1cfbbd4de617af15cfa71d9847edc462d5
AMX <amxgamer24ff@gmail.com>
2025-07-10 04:12:18 +0000

    swiching from home to other bar taking so much time . do it instant reac

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -91,10 +91,10 @@ export function AppShell({ children }: { children: React.ReactNode }) {
               <AnimatePresence mode="wait">
                   <motion.div
                       key={pathname}
-                      initial={{ opacity: 0, y: 10 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      exit={{ opacity: 0, y: -10 }}
-                      transition={{ duration: 0.15 }}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      transition={{ duration: 0.05 }}
                   >
                       {children}
                   </motion.div>
