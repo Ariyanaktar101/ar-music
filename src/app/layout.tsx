2d8f2125b11b41d45b8d9932fb35f0cd477f3d8c
AMX <amxgamer24ff@gmail.com>
2025-07-22 07:21:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from '@/context/AuthContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { SettingsProvider } from '@/context/SettingsContext';
 import { AppShell } from '@/components/app-shell';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 export const metadata: Metadata = {
   title: 'AR Music',
@@ -25,16 +26,18 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Sacramento&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <SettingsProvider>
-          <ThemeProvider>
-            <AuthProvider>
-              <MusicPlayerProvider>
-                <AppShell>{children}</AppShell>
-                <Toaster />
-              </MusicPlayerProvider>
-            </AuthProvider>
-          </ThemeProvider>
-        </SettingsProvider>
+        <TooltipProvider>
+          <SettingsProvider>
+            <ThemeProvider>
+              <AuthProvider>
+                <MusicPlayerProvider>
+                  <AppShell>{children}</AppShell>
+                  <Toaster />
+                </MusicPlayerProvider>
+              </AuthProvider>
+            </ThemeProvider>
+          </SettingsProvider>
+        </TooltipProvider>
       </body>
     </html>
   );
